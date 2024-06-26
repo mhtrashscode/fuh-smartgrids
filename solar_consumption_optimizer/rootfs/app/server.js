2e247f534e18a889bf2e9461cb6c125ffef71868
Max <max.herwig@siemens.com>
2024-06-26 18:10:53 +0200

    added solar consumption optimizer addon

---
+++ solar_consumption_optimizer/rootfs/app/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+
+app.get("*", function (req, res) {
+    res.send("Express Fallback Route");   
+});
+
+app.listen(port, () => {
+  console.log(`Demo app listening on port ${port}`)
+});
---
+++ solar_consumption_optimizer/run.sh
@@ -0,0 +1,10 @@
+# this script is executed in context of /app
+# debug
+pwd
+whoami
+node --version
+#tree
+# run web server
+npm run start
+# keep container alive
+#sleep infinity
